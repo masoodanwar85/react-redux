4710ff86e1e2d634c365a9358b3816db3cc4770c
Masood Anwar <masood@octadyne.com>
2020-05-11 03:37:06 +0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
+import Posts from './components/Posts';
+import PostForm from './components/PostForm';
+
+function App() {
+    return (
+        <Provider store={store}>
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                </header>
+                <PostForm />
+                <hr />
+                <Posts />
+            </div>
+        </Provider>
+  );
+}
+
+export default App;
