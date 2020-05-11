d5f7cfeb5adb8283fe93e7962940d9b3d9342e66
Masood Anwar <masoodanwar85@gmail.com>
2020-05-11 08:00:45 +0500

    Added Create Post

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -8,3 +8,18 @@ export const fetchPosts = () => dispatch => {
         payload: posts,
     }));
 }
+
+export const createPost = (postData) => dispatch => {
+	fetch('https://jsonplaceholder.typicode.com/posts', {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json'
+		},
+		body: JSON.stringify(postData)
+	})
+	.then(res => res.json())
+	.then(data => dispatch({
+		type: NEW_POST,
+		payload: postData
+	}));
+}
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -9,6 +9,15 @@ class Posts extends React.Component {
         this.props.fetchPosts();
     }
 
+	static getDerivedStateFromProps(props,state) {
+		console.log(props);
+		console.log(state);
+		return this.props;
+		// if (nextProps.newPost) {
+		// 	this.props.posts.unshift(nextProps.newPost);
+		// }
+	}
+
     render () {
         var postItems = this.props.posts.map(post => (
             <div key={post.id}>
@@ -27,10 +36,12 @@ class Posts extends React.Component {
 
 Posts.propTypes = {
     fetchPosts: PropTypes.func.isRequired,
-    posts: PropTypes.array.isRequired
+    posts: PropTypes.array.isRequired,
+	newPost: PropTypes.object
 }
 
 const mapStateToProps = state => ({
-    posts: state.posts.items
+    posts: state.posts.items,
+	newPost: state.posts.item
 });
 export default connect(mapStateToProps, {fetchPosts}) (Posts);
